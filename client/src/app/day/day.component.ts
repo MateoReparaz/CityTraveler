cab840999e2d823a8bfc0966a8fdf9f81898cb41
MateoReparaz <mateoreparaz1989@gmail.com>
2018-08-08 19:16:09 +0200

    directions implementado, puliendo waypoints distancia y tiempo

--- client/src/app/day/day.component.ts
+++ client/src/app/day/day.component.ts
@@ -1,4 +1,5 @@
-import { TripsService } from './../../services/trips.service';
+import { LoginComponent } from "./../login/login.component";
+import { TripsService } from "./../../services/trips.service";
 import { ActivatedRoute } from "@angular/router";
 import { DayService } from "./../../services/day.service";
 import { Component, OnInit } from "@angular/core";
@@ -14,49 +15,72 @@ export class DayComponent implements OnInit {
   save: any;
   lat: number;
   lng: number;
-  trip:any
+  trip: any;
 
   public origin: {};
   public destination: {};
   public renderOptions = {
     draggable: true
   };
-  public waypoints: any 
-  public travelMode: string = 'WALKING'
-  public change(event: any) {
+  /* public waypoints: any; */
+  public travelMode: string = "WALKING";
+  /* public change(event: any) {
     this.waypoints = event.request.waypoints;
-  } 
+  } */
+  public optimizeWaypoints: boolean = true
 
-  constructor(public dayService: DayService, private route: ActivatedRoute,public tripsService: TripsService) {}
+  constructor(
+    public dayService: DayService,
+    private route: ActivatedRoute,
+    public tripsService: TripsService
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.dayService.getDay(params.idDay, params.idTrip).subscribe(day => {
         this.day = day;
-        this.waypoints = [
-          /* {
+        /* this.waypoints = [
+           {
             location: { lat: this.day.pois[1].location.lat, lng: this.day.pois[1].location.lng },
             stopover:false,
         },
         {
           location: { lat: this.day.pois[2].location.lat, lng: this.day.pois[2].location.lng },
           stopover: false,
-      } */
-        ];
+      } 
+        ]; */
         this.getDirection();
       });
     });
   }
-  
+
   getDirection() {
-    this.origin = {
-      lat: this.day.pois[0].location.lat,
-      lng: this.day.pois[0].location.lng
-    };
-    this.destination = {
-      lat: this.day.pois[this.day.pois.length - 1].location.lat,
-      lng: this.day.pois[this.day.pois.length - 1].location.lng
-    };
+    let maxX = -1000;
+    let minX = 1000;
+    let maxY = -1000;
+    let minY = 1000;
+    this.day.pois.forEach(element => {
+      if (element.location.lng > maxX) {
+        maxX = element.location.lng;
+      }
+      if (element.location.lng < minX) {
+        minX = element.location.lng;
+      }
+      if (element.location.lat > maxY) {
+        maxY = element.location.lat;
+      }
+      if (element.location.lat < minY) {
+        minY = element.location.lat;
+      }
+    });
+    if (maxX - minX > maxY - minY) {
+      this.origin = this.day.pois.find(e => e.location.lng == minX).location;
+      this.destination = this.day.pois.find(e => e.location.lng == maxX).location;
+    } else {
+      this.origin = this.day.pois.find(e => e.location.lat == minY).location;
+      this.destination = this.day.pois.find(e => e.location.lat == maxY).location;
+    }
+    console.log(this.origin, this.destination)
   }
 
   saveOpen(i: any) {
@@ -86,14 +110,13 @@ export class DayComponent implements OnInit {
       this.tripsService.deletePoi(index, params.idTrip, day).subscribe(() => {
         this.update();
       });
-    })
+    });
   }
   update() {
     this.route.params.subscribe(params =>
-      this.dayService.getDay(params.idDay,params.idTrip).subscribe(day => {
+      this.dayService.getDay(params.idDay, params.idTrip).subscribe(day => {
         this.day = day;
       })
     );
   }
-  
 }
